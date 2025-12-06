376f97569522f9dd8fccddb98bfef6a91562b951
Joost Boomkamp <joost.boomkamp@teamrockstars.nl>
2025-12-06 19:27:58 +0100

    Day 3

---
+++ Day03/Day03.cs
@@ -0,0 +1,88 @@
+﻿using System.Text;
+
+namespace Aoc2025.Day03;
+
+internal class Day03
+{
+    List<string> BatteryBanks = [];
+
+    public Day03(string input)
+    {
+        // 987654321111111 98
+        // 811111111111119 89
+        // 234234234234278 78
+        // 818181911112111 92
+
+        BatteryBanks = input.Split(Environment.NewLine).ToList();
+    }
+
+    public Int128 Run(bool simpleMethod = true)
+    {
+        if (!simpleMethod)
+            return RunHard();
+
+        var scores = BatteryBanks
+            .Select(FindHigestJolt)
+            .ToArray();
+
+        return scores.Sum();
+    }
+
+    private Int128 RunHard()
+    {
+        var scores = BatteryBanks
+            .Select(Find12HigestJolts)
+            .ToArray();
+
+        return scores.Sum();
+    }
+
+    private static long FindHigestJolt(string input)
+    {
+        var leading = FindHighestJoltFrom(input, 0, 1);
+        var second = FindHighestJoltFrom(input, leading.Index + 1, 0);
+
+        return long.Parse($"{leading.Value}{second.Value}");
+    }
+
+    private static long Find12HigestJolts(string input)
+    {
+        int index = -1;
+        char value = '0';
+
+        var number = new StringBuilder();
+
+        for (var i = 0; i < 12; i++)
+        {
+            (index, value) = FindHighestJoltFrom(input, index + 1, 11-i);
+            number.Append(value);
+        }
+        return long.Parse(number.ToString());
+    }
+
+    private static (int Index, char Value) FindHighestJoltFrom(string input, int startIndex, int endCutoff) 
+    {
+        // for the first decimal, we cannot use the last character. cut off the last character, we could end up with 90
+        var removedFront = input.Substring(0, startIndex);
+        var selection = input.Substring(startIndex, input.Length - startIndex - endCutoff);
+        var maximum = selection.Length - 1;
+
+        var highestPosition = startIndex;
+        var highest = input[startIndex];
+
+        for (var index = startIndex; index <= startIndex + maximum; index++)
+        {
+            if (highest == '9')
+            {
+                break; // no polong in finding anything higher than the maximum
+            }
+            if (input[index] > highest)
+            {
+                highest = input[index];
+                highestPosition = index;
+            }
+        }
+
+        return (highestPosition, highest); 
+    }
+}
---
+++ Day03/Day03Test.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+
+namespace Aoc2025.Day03;
+
+internal class Day03Test
+{
+    [Test]
+    public void RunSample1() 
+    {
+        // arrange
+        var day = new Day03(Day03Input.Sample);
+
+        // act
+        var result = day.Run();
+
+        // assert
+        Assert.That(result == 357);
+    }
+
+    [Test]
+    public void RunActual1()
+    {
+        // arrange
+        var day = new Day03(Day03Input.Actual);
+
+        // act
+        var result = day.Run();
+
+        // assert
+        Assert.That(result == 17193);
+    }
+
+    [Test]
+    public void RunSample2()
+    {
+        // arrange
+        var day = new Day03(Day03Input.Sample);
+
+        // act
+        var result = day.Run(false);
+
+        // assert
+        // The total output joltage is now much larger: 987654321111 + 811111111119 + 434234234278 + 888911112111 = 3121910778619.
+        Assert.That(result == 3121910778619);
+    }
+
+    [Test]
+    public void RunActual2()
+    {
+        // arrange
+        var day = new Day03(Day03Input.Actual);
+
+        // act
+        var result = day.Run(false);
+
+        // assert
+        Assert.That(result == 171297349921310);
+    }
+}
