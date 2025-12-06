fa6d3a1342438baa5b865faa9159d1cab670afcf
Joost Boomkamp <joost.boomkamp@teamrockstars.nl>
2025-12-06 19:27:50 +0100

    Cleanup Day 02

--- Day02/Day02.cs
+++ Day02/Day02.cs
@@ -67,30 +67,3 @@ internal class Day02
         }
     }
 }
-
-internal class ValueRange
-{
-    public long Start { get; private set; }
-    public long End { get; private set; }
-
-    public static ValueRange Parse (string input)
-    {
-        var items = input.Split ('-');
-        return new ValueRange
-        {
-            Start = long.Parse(items[0]),
-            End = long.Parse(items[1])
-        };
-    }
-
-    public IEnumerable<long> Filter(Func<long, bool> action)
-    {
-        for (var i = Start; i <= End; i++)
-        {
-            if (action(i))
-            {
-                yield return i;
-            }
-        }
-    }
-}
