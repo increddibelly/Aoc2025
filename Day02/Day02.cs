fcd5ecc299d1d3d554669e3b9b4c7a8ab788fcfa
Joost Boomkamp <joost.boomkamp@teamrockstars.nl>
2025-12-06 18:07:22 +0100

    Day02

---
+++ Day02/Day02.cs
@@ -0,0 +1,96 @@
+﻿namespace Aoc2025.Day02;
+
+internal class Day02
+{
+    // you can find the invalid IDs by looking for any ID which is made only of some sequence of digits repeated twice.
+    // So, 55 (5 twice), 6464 (64 twice), and 123123 (123 twice) would all be invalid IDs.
+    public ValueRange[] IdRanges { get; }
+
+    public Day02(string sample)
+    {
+        var inputRanges = sample.Split(',');
+        IdRanges = inputRanges.Select(ValueRange.Parse).ToArray();
+    }
+
+    public long Run(bool simpleMethod = true)
+    {
+        var badIds = new List<long>();
+        foreach (var range in IdRanges)
+        {
+            badIds.AddRange(range.Filter(simpleMethod ? IsInvalidId : IsMoreInvalidId));
+        }
+        return badIds.Sum();
+    }
+
+    private bool IsInvalidId(long id)
+    {
+        var idString = id.ToString();
+
+        // a bad ID consists of two identical halves. odd lengths cannot be identical
+        if (idString.Length % 2 == 1)
+        {
+            return false;
+        }
+
+        var secondHalf = idString.Substring(idString.Length / 2);
+        if (idString.StartsWith(secondHalf))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool IsMoreInvalidId(long id)
+    {
+        var idString = id.ToString();
+        var target = idString.Length / 2;
+        
+        // an ID is invalid if it is made only of some sequence of digits repeated at least twice.
+        // So, 12341234 (1234 two times), 123123123 (123 three times), 1212121212 (12 five times),
+        // and 1111111 (1 seven times) are all invalid IDs.
+        for (var length = 1; length <= target; length++)
+        {
+            var pattern = idString.Substring(0, length);
+            if (!ContainsOtherCharacters(idString, pattern))
+            {
+                return true; // found an ID that contains no other characters than the search pattern
+            }
+        }
+
+        return false;
+
+        static bool ContainsOtherCharacters(string input, string pattern)
+        {
+            var haystack = new string(input);
+            return haystack.Replace(pattern, string.Empty).Length > 0;
+        }
+    }
+}
+
+internal class ValueRange
+{
+    public long Start { get; private set; }
+    public long End { get; private set; }
+
+    public static ValueRange Parse (string input)
+    {
+        var items = input.Split ('-');
+        return new ValueRange
+        {
+            Start = long.Parse(items[0]),
+            End = long.Parse(items[1])
+        };
+    }
+
+    public IEnumerable<long> Filter(Func<long, bool> action)
+    {
+        for (var i = Start; i <= End; i++)
+        {
+            if (action(i))
+            {
+                yield return i;
+            }
+        }
+    }
+}
---
+++ Day02/Day02Test.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+
+namespace Aoc2025.Day02;
+
+internal class Day02Test
+{
+    [Test]
+    public void RunSample1() 
+    {
+        // arrange
+        var day = new Day02(Day02Input.Sample);
+
+        // act
+        var result = day.Run();
+
+        // assert
+        Assert.That(result == 1227775554);
+    }
+
+    [Test]
+    public void RunActual1()
+    {
+        // arrange
+        var day = new Day02(Day02Input.Actual);
+
+        // act
+        var result = day.Run();
+
+        // assert
+        Assert.That(result == 43952536386);
+    }
+
+    [Test]
+    public void RunSample2()
+    {
+        // arrange
+        var day = new Day02(Day02Input.Sample);
+
+        // act
+        var result = day.Run(false);
+
+        // assert
+        Assert.That(result == 4174379265);
+    }
+
+    [Test]
+    public void RunActual2()
+    {
+        // arrange
+        var day = new Day02(Day02Input.Actual);
+
+        // act
+        var result = day.Run(false);
+
+        // assert
+        Assert.That(result == 54486209192);
+    }
+}
